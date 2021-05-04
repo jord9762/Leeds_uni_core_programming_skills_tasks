c71b16f7e3181689795ecdb9595b62abe16c031b
Jordan Reynoldson <68658177+jord9762@users.noreply.github.com>
2021-05-04 15:52:40 +0100

    Update model.py

--- Task1_agent_based_modelling/model.py
+++ Task1_agent_based_modelling/model.py
@@ -1,24 +1,25 @@
-import random 
 #importing the random package
+import random 
+#assigning the integer 50 to both of the above variables 
 y0 = 50
 x0 = 50
-#assigning 50 to both of the above variables 
+#either subtracts by one or adds by one at random, adding if less than 0.5 
+#subtracting if greater than 0.5, in this sense it is not truly random. 
 if random.random() < 0.5:
     y0 += 1
 else:
     y0 -= 1
-#basically either subtracting one or adding one at random, adding if less than 0.5 
-#subtracting if greater than 0.5
+
 if random.random() < 0.5:
     x0 += 1
 else:
     x0 -= 1
-#does the same as previous
+#prints variables after being randomised
 print(y0, x0)
-#prints
 
 
 
+#lines 23 to 36 repeat the previous process for a second set of co-ordinates.
 y1 = 50
 x1 = 50
 
@@ -33,12 +34,12 @@ else:
     x1 -= 1
 
 print(y1, x1)
-#Whole equation I initially came up with but below may be a more sensible way to calculate
+#Whole equation I initially came up with, efficent but potentially difficult to understand. 
 answer = (((y0 - y1)**2) + ((x0 - x1)**2))**0.5 
 print (answer)
 
 
-
+#Below may be a more sensible way to calculate distance
 y0 = random.randint(0,100)
 x0 = random.randint(0,100)
 y1 = random.randint(0,100)
@@ -51,4 +52,4 @@ x_diffsq = x_diff * x_diff
 sum = y_diffsq + x_diffsq
 answer = sum**0.5
 print(answer)
-#alternate way the distance could have been calculated. 
+
