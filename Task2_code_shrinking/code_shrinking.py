f6290a6377cf705967df4bdf6bc8d4ca92b7d538
Jordan Reynoldson <68658177+jord9762@users.noreply.github.com>
2021-05-04 16:46:15 +0100

    Update code_shrinking.py

--- Task2_code_shrinking/code_shrinking.py
+++ Task2_code_shrinking/code_shrinking.py
@@ -1,12 +1,14 @@
 import random
 import operator
 import matplotlib.pyplot
-
+#specifies the number of agents which will be created for the variable num_of_agents
 num_of_agents = 10
+#specifies the number of iterations or in other words the amount of times the code will be run
 num_of_iterations = 100
+#creates an empty list for agents
 agents = []
 
-# Make the agents.
+# Make the agents. Creates 10 agents randomly. .Append adds element to the end of the list. 
 for i in range(num_of_agents):
     agents.append([random.randint(0,99),random.randint(0,99)])
 
@@ -28,9 +30,11 @@ for j in range(num_of_iterations):
 answer = (((agents[0][0] - agents[1][0])**2) + ((agents[0][1] - agents[1][1])**2))**0.5
 print(answer)
 '''
-
+#plots agents on a pyplot, this is done for all agents with the foor loop present on line 37. [i][1] represents the Y of the agents co-ordinate and [i][0] represents the X.
 matplotlib.pyplot.ylim(0, 99)
 matplotlib.pyplot.xlim(0, 99)
+
 for i in range(num_of_agents):
     matplotlib.pyplot.scatter(agents[i][1],agents[i][0])
+    #code on line 40 plots the agents created in this script
 matplotlib.pyplot.show()
